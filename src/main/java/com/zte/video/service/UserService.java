6f6e0a1a6539930f63710df9a9fba65b9b55baef
qwerqqwewq <2725543850@qq.com>
2019-06-25 13:40:09 +0800

    Merge branch 'master' of https://github.com/qwerqqwewq/VideoOnline
    # Conflicts:
    #	src/main/java/com/zte/video/entity/Power.java

--- src/main/java/com/zte/video/service/UserService.java
+++ src/main/java/com/zte/video/service/UserService.java
@@ -1,9 +1,12 @@
 package com.zte.video.service;
 
 /**
- * Author:helloboy
- * Date:2019-06-25 10:14
+ * @Author:helloboy
+ * @Date:2019-06-25 10:14
  * Description:<描述>
  */
 public interface UserService {
+    /**
+     *
+     */
 }
