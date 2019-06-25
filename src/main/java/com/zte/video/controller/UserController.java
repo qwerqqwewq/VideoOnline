566d79085658adff8a8cc50d6d40f349ab5dcd84
qwerqqwewq <2725543850@qq.com>
2019-06-25 16:35:07 +0800

    Merge branch 'master' of https://github.com/qwerqqwewq/VideoOnline
    # Conflicts:
    #	src/main/java/com/zte/video/entity/Power.java

--- src/main/java/com/zte/video/controller/UserController.java
+++ src/main/java/com/zte/video/controller/UserController.java
@@ -1,10 +1,28 @@
 package com.zte.video.controller;
 
+import com.zte.video.service.PowerService;
+import com.zte.video.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @Author:helloboy
  * @Date:2019-06-25 10:14
  * Description:<描述>
  */
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PowerService powerService;
+
+    /**
+     * 判断用户名是否重复
+     */
+    private
 }
