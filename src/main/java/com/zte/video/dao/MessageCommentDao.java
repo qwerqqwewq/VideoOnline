8ec2255beeaba7b50b4940649594a2b14e33ebc9
qwerqqwewq <2725543850@qq.com>
2019-06-25 11:53:10 +0800

    Merge branch 'master' of https://github.com/qwerqqwewq/VideoOnline
    # Conflicts:
    #	src/main/java/com/zte/video/entity/Power.java

--- src/main/java/com/zte/video/dao/MessageCommentDao.java
+++ src/main/java/com/zte/video/dao/MessageCommentDao.java
@@ -1,6 +1,9 @@
 package com.zte.video.dao;
 
+import com.zte.video.entity.Message;
 import com.zte.video.entity.MessageComment;
+
+import java.util.Set;
 
 /**
  * Author:helloboy
@@ -10,22 +13,23 @@ import com.zte.video.entity.MessageComment;
 public interface MessageCommentDao {
     /**
      * 添加对某个留言的回复
-     * @param mid
-     * @param uid
+     * @param message
      * @return
      */
-    public MessageComment insertMessageComment();
+    public MessageComment insertMessageCommentById(Message message);
 
     /**
      * 查询某个留言下的所有的回复
+     * @param message
+     * @return
      */
-    public void selectByID(Integer mid);
+    public Set<MessageComment> selectAllByID(Message message);
 
     /**
-     * 删除此留言下的某个回复
+     * 删除某条留言下的某个回复
      * @param id
      * @return
      */
-    public MessageComment deleteMessageComment(Integer id);
+    public MessageComment deleteMessageCommentByID(Integer id);
 
 }
