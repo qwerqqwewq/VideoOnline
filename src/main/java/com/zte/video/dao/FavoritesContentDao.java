a8a08f312e0ae65958f7c475f522314277aa9710
qwerqqweqw <2725543850@qq.com>
2019-06-25 15:19:39 +0800

    Merge branch 'master' of https://github.com/qwerqqwewq/VideoOnline
    # Conflicts:
    #	src/main/java/com/zte/video/dao/MessageCommentDao.java

--- src/main/java/com/zte/video/dao/FavoritesContentDao.java
+++ src/main/java/com/zte/video/dao/FavoritesContentDao.java
@@ -1,9 +1,43 @@
 package com.zte.video.dao;
 
+import com.zte.video.entity.Favorites;
+import com.zte.video.entity.FavoritesContent;
+
+import java.util.List;
+
 /**
  * Author:helloboy
  * Date:2019-06-25 14:51
  * Description:<描述>
  */
 public interface FavoritesContentDao {
+    /**
+     * 查询所有收藏夹内容
+     * @return
+     */
+    public List<FavoritesContent> selectAll();
+    /**
+     * 根据收藏夹查询内容
+     * @param favorites
+     * @return
+     */
+    public List<FavoritesContent> selectAllById(Favorites favorites);
+    /**
+     * 添加新收藏夹内容
+     * @param favoritesContent
+     * @return
+     */
+    public void insertFavoritesContent(FavoritesContent favoritesContent);
+    /**
+     * 修改收藏夹内容
+     * @param favoritesContent
+     * @return
+     */
+    public void updateFavoritesContent(FavoritesContent favoritesContent);
+    /**
+     * 删除收藏夹内容
+     * @param favoritesContent
+     * @return
+     */
+    public void deleteFavoritesContent(FavoritesContent favoritesContent);
 }
